6467696e876345be9c43710c25491dc9fc0ac306
TLTwizz <etienne.ducros987@gmail.comm>
2024-05-24 15:06:33 +0200

    integrate socket.io with next.js 6

--- server/server.js
+++ server/server.js
@@ -1,84 +1,83 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
-const { Server } = require("socket.io");
+// server.js
+
+const http = require("http");
+const { Server } = require("socket.io"); //import server class from socket.io
 const { PrismaClient } = require("@prisma/client");
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
-const prisma = new PrismaClient();
+const prisma = new PrismaClient(); //instanciate prisma client
+const server = http.createServer();
 
-app.prepare().then(() => {
-    const server = createServer((req, res) => {
-        const parsedUrl = parse(req.url, true);
-        handle(req, res, parsedUrl);
+// Add cors option to allow cross-origin requests
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+// Manage socket.io connections
+io.on("connection", async (socket) => {
+    console.log("User connected");
+    let restorePosts = await prisma.post.findMany(); //get all posts from database
+    io.emit("restorePosts", restorePosts); // send all posts to the client
+
+    socket.on("newPost", async (newPost) => {
+        // listen to newPost event
+        try {
+            console.log("newPost", newPost);
+            // save new Post to the database using Prisma
+            await prisma.post.create({
+                data: {
+                    id: newPost.id,
+                    title: newPost.title,
+                    description: newPost.description,
+                    authorId: newPost.authorId,
+                    lats: newPost.lats,
+                    lngs: newPost.lngs,
+                    elevations: newPost.elevations,
+                    category: newPost.category,
+                    length: newPost.length,
+                    difficulty: newPost.difficulty,
+                },
+            });
+
+            // send all posts to all connected clients
+            posts = await prisma.post.findMany();
+            io.emit("restorePosts", posts);
+        } catch (error) {
+            console.error("Error saving Post:", error);
+        }
     });
 
-    const io = new Server(server, {
-        cors: {
-            origin: "*", //process.env.WEB_ORIGIN,
-            methods: ["GET", "POST"],
-        },
-    });
+    socket.on("deletePost", async (postId) => {
+        // delete post from database
+        try {
+            console.log("deletePost", postId);
 
-    io.on("connection", async (socket) => {
-        console.log("User connected");
-        let restorePosts = await prisma.post.findMany();
-        io.emit("restorePosts", restorePosts);
-
-        socket.on("newPost", async (newPost) => {
-            try {
-                console.log("newPost", newPost);
-                await prisma.post.create({
-                    data: {
-                        id: newPost.id,
-                        title: newPost.title,
-                        description: newPost.description,
-                        authorId: newPost.authorId,
-                        lats: newPost.lats,
-                        lngs: newPost.lngs,
-                        elevations: newPost.elevations,
-                        category: newPost.category,
-                        length: newPost.length,
-                        difficulty: newPost.difficulty,
-                    },
-                });
-
-                posts = await prisma.post.findMany();
-                io.emit("restorePosts", posts);
-            } catch (error) {
-                console.error("Error saving Post:", error);
+            if (!postId || typeof postId !== "string") {
+                throw new Error("Invalid Id");
             }
-        });
 
-        socket.on("deletePost", async (postId) => {
-            try {
-                console.log("deletePost", postId);
+            await prisma.post.delete({
+                where: { id: postId },
+            });
 
-                if (!postId || typeof postId !== "string") {
-                    throw new Error("Invalid Id");
-                }
-
-                await prisma.Post.delete({
-                    where: { id: postId },
-                });
-
-                const newPosts = await prisma.post.findMany();
-                io.emit("restorePosts", newPosts);
-            } catch (error) {
-                console.error("Error deleting Post:", error);
-            }
-        });
-
-        socket.on("disconnect", () => {
-            console.log("User disconnected");
-        });
+            // uptade posts with the new one
+            const newPosts = await prisma.post.findMany();
+            // send all posts to all connected clients
+            io.emit("restorePosts", newPosts);
+        } catch (error) {
+            console.error("Error deleting Post:", error);
+        }
     });
 
-    const PORT = 3001; //parseInt(process.env.PORT, 10) || 3000;
-    server.listen(PORT, (err) => {
-        if (err) throw err;
-        console.log(`Server running on port :${PORT}`);
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
     });
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+    //start server on port 3001
+    console.log(`Server running on port ${PORT}`);
 });
