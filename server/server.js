769114541aeb631eec67d0c3f65feefc89144ad1
TLTwizz <etienne.ducros987@gmail.comm>
2024-05-24 15:27:28 +0200

    integrate socket.io with next.js 8

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ io.on("connection", async (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
