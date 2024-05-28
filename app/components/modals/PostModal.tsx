9cf7674f57473ce778200e94aa599bef1f8de5ed
TLTwizz <etienne.ducros987@gmail.comm>
2024-05-28 09:43:29 +0200

    maj redirect post après création 3

--- app/components/modals/PostModal.tsx
+++ app/components/modals/PostModal.tsx
@@ -236,7 +236,6 @@ const PostModal: React.FC<PostModalProps> = ({ socket, currentUser }) => {
             socket?.emit("newPost", data);
             toast.success("Post créé !");
             router.refresh();
-            router.push(`/posts/${data.id}`); // redirect to the new post
         } catch (error) {
             toast.error("Une erreur s'est produite");
         }
@@ -245,6 +244,8 @@ const PostModal: React.FC<PostModalProps> = ({ socket, currentUser }) => {
         setStep(STEPS.FILE); // reset the step to the first one
         postModal.onClose();
         setIsLoading(false);
+        router.push(`/posts/${data.id}`); // redirect to the new post
+        location.reload();
     };
 
     let bodyContent = (
