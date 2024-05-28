fc2937b370786d4488907c3f3ae56fb36e74a965
TLTwizz <etienne.ducros987@gmail.comm>
2024-05-28 09:22:52 +0200

    maj redirect post après création

--- app/components/modals/PostModal.tsx
+++ app/components/modals/PostModal.tsx
@@ -244,6 +244,7 @@ const PostModal: React.FC<PostModalProps> = ({ socket, currentUser }) => {
         setStep(STEPS.FILE); // reset the step to the first one
         postModal.onClose();
         setIsLoading(false);
+        router.push(`/posts/${data.id}`); // redirect to the new post
     };
 
     let bodyContent = (
